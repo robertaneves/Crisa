25a8f0d7a7f2ed91d390b231b051e8064dc57e2e
Roberta Neves <roberta.neves76@hotmail.com>
2025-08-14 18:14:00 -0300

    web server

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-// server.js
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
+// Servir arquivos estáticos da pasta dist
 app.use(express.static(path.join(__dirname, "dist")));
 
+// Rota coringa para SPA (sempre devolver index.html)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
